43bfea989b8a692995dec51a4c9fd554523e0613
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-04-20 08:21:29 +0000

    svn log -r75027 https://source.sakaiproject.org/svn/kernel/trunk ------------------------------------------------------------------------ r75027 | david.horwitz@uct.ac.za | 2010-03-23 15:38:55 +0200 (Tue, 23 Mar 2010) | 1 line
    KNL-450
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-4:~/branchManagement/kernel-1.0.x> svn merge
    -c75027 https://source.sakaiproject.org/svn/kernel/trunk
    --- Merging r75027 into '.':
    U
    kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/kernel@76365 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- branches/kernel-1.0.x/kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
+++ branches/kernel-1.0.x/kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -2425,16 +2425,24 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		String currentUser = SessionManager.getCurrentSessionUserId();
 		String owner = "";
       
+		if (m_securityService.isSuperUser(currentUser)) {
+			//supper users should always get a 404 rather than a permission exception
+			return true;
+		}
+		
 		try
 		{
  			ResourceProperties props = getProperties(id);
 			owner = props.getProperty(ResourceProperties.PROP_CREATOR);
 		}
-		catch ( Exception e ) 
+		catch (PermissionException e ) 
 		{
 			// PermissionException can be thrown if not AUTH_RESOURCE_READ
 			return false;
-		}
+		} catch (IdUnusedException e) {
+			//Also non admin users should get a permission exception is the resource doesn't exist
+			return false;
+		} 
       
 		// check security to delete any collection
 		if ( unlockCheck(AUTH_RESOURCE_WRITE_ANY, id) )
